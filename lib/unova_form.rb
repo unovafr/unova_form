0cfa16b63a83d8f57f5bbed1c55a07b323515645
William Richer <william.richer@unova.fr>
2023-09-01 12:10:28 +0200

    init

---
+++ lib/unova_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "unova_form/version"
+
+module UnovaForm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/unova_form_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UnovaForm do
+  it "has a version number" do
+    expect(UnovaForm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
