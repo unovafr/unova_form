d141eaa1e43bc23d0d9dfdee68d3f9c08d2e72c5
William Richer <william.richer@unova.fr>
2023-09-05 14:36:55 +0200

    css generator building...

--- lib/generators/unova_form/assets/assets_generator.rb
+++ lib/generators/unova_form/assets/assets_generator.rb
@@ -38,76 +38,76 @@ class UnovaForm::AssetsGenerator < Rails::Generators::NamedBase
         destination = nil unless File.directory?(destination)
       end
 
-      theme = {}
+      @theme = {}
       case framework
         when "tailwind"
-          theme[:label_color] = "color-gray-700"
-          theme[:label_font_size] = "text-sm"
-          theme[:label_font_weight] = "font-normal"
-          theme[:label_font_family] = "font-sans"
-          theme[:label_margin_bottom] = "mb-1"
-          theme[:input_border_color] = "border-gray-300"
-          theme[:input_border_width] = "border"
-          theme[:input_border_radius] = "rounded-md"
-          theme[:input_padding] = "px-3 py-2"
-          theme[:input_color] = "text-gray-900"
-          theme[:input_font_size] = "text-base"
-          theme[:input_font_weight] = "font-normal"
-          theme[:input_font_family] = "font-sans"
-          theme[:input_background_color] = "bg-white"
-          theme[:input_placeholder_color] = "placeholder-gray-500"
-          theme[:input_focus_border_color] = "focus:border-blue-500"
-          theme[:error_color] = "text-red-500"
-          theme[:error_font_size] = "text-sm"
-          theme[:error_font_weight] = "font-normal"
-          theme[:error_font_family] = "font-sans"
-          theme[:error_margin_top] = "mt-1"
-          theme[:error_margin_bottom] = "mb-1"
+          @theme[:label_color] = "color-gray-700"
+          @theme[:label_font_size] = "text-sm"
+          @theme[:label_font_weight] = "font-normal"
+          @theme[:label_font_family] = "font-sans"
+          @theme[:label_margin_bottom] = "mb-1"
+          @theme[:input_border_color] = "border-gray-300"
+          @theme[:input_border_width] = "border"
+          @theme[:input_border_radius] = "rounded-md"
+          @theme[:input_padding] = "px-3 py-2"
+          @theme[:input_color] = "text-gray-900"
+          @theme[:input_font_size] = "text-base"
+          @theme[:input_font_weight] = "font-normal"
+          @theme[:input_font_family] = "font-sans"
+          @theme[:input_background_color] = "bg-white"
+          @theme[:input_placeholder_color] = "placeholder-gray-500"
+          @theme[:input_focus_border_color] = "focus:border-blue-500"
+          @theme[:error_color] = "text-red-500"
+          @theme[:error_font_size] = "text-sm"
+          @theme[:error_font_weight] = "font-normal"
+          @theme[:error_font_family] = "font-sans"
+          @theme[:error_margin_top] = "mt-1"
+          @theme[:error_margin_bottom] = "mb-1"
         else
-          theme[:label_color] = "#000000"
-          theme[:label_font_size] = "1rem"
-          theme[:label_font_weight] = "400"
-          theme[:label_font_family] = "sans-serif"
-          theme[:label_margin_bottom] = "0.25rem"
-          theme[:input_border_color] = "#000000"
-          theme[:input_border_width] = "1px"
-          theme[:input_border_radius] = "0.25rem"
-          theme[:input_padding] = "0.375rem 0.75rem"
-          theme[:input_color] = "#000000"
-          theme[:input_font_size] = "1rem"
-          theme[:input_font_weight] = "400"
-          theme[:input_font_family] = "sans-serif"
-          theme[:input_background_color] = "#ffffff"
-          theme[:input_placeholder_color] = "#6c757d"
-          theme[:input_focus_border_color] = "#000000"
-          theme[:error_color] = "#dc3545"
-          theme[:error_font_size] = "0.875rem"
-          theme[:error_font_weight] = "400"
-          theme[:error_font_family] = "sans-serif"
-          theme[:error_margin_top] = "0.25rem"
-          theme[:error_margin_bottom] = "0.25rem"
+          @theme[:label_color] = "#000000"
+          @theme[:label_font_size] = "1rem"
+          @theme[:label_font_weight] = "400"
+          @theme[:label_font_family] = "sans-serif"
+          @theme[:label_margin_bottom] = "0.25rem"
+          @theme[:input_border_color] = "#000000"
+          @theme[:input_border_width] = "1px"
+          @theme[:input_border_radius] = "0.25rem"
+          @theme[:input_padding] = "0.375rem 0.75rem"
+          @theme[:input_color] = "#000000"
+          @theme[:input_font_size] = "1rem"
+          @theme[:input_font_weight] = "400"
+          @theme[:input_font_family] = "sans-serif"
+          @theme[:input_background_color] = "#ffffff"
+          @theme[:input_placeholder_color] = "#6c757d"
+          @theme[:input_focus_border_color] = "#000000"
+          @theme[:error_color] = "#dc3545"
+          @theme[:error_font_size] = "0.875rem"
+          @theme[:error_font_weight] = "400"
+          @theme[:error_font_family] = "sans-serif"
+          @theme[:error_margin_top] = "0.25rem"
+          @theme[:error_margin_bottom] = "0.25rem"
       end
 
-      # say theme and ask if want to edit
-      say "Here is your theme: #{theme.map { |k, v| "#{k}: #{v}" }.join('\n')}"
+      # say @theme and ask if want to edit
+      say "Here is your @theme: #{@theme.map { |k, v| "#{k}: #{v}" }.join('\n')}"
       if yes?("Do you want to edit it? (y/n)")
         # ask if step by step is wanted or not
         if yes?("Do you want to edit it step by step? (y/n), if you answer no, you will be asked if you want to edit individual values.")
-          theme.each do |k, v|
+          @theme.each do |k, v|
             ans = ask("#{k}: (#{v})")
-            theme[k] = ans if ans.present?
+            @theme[k] = ans if ans.present?
           end
         else
           # ask if want to edit individual values
-          if yes?("Do you want to edit any theme value?")
+          if yes?("Do you want to edit any @theme value?")
             el_editing = "notBlank"
             while el_editing.present?
               # ask for element to edit
-              el_editing = ask("What element do you want to edit? (#{theme.keys.join(', ')}) leave blank to stop editing")
+              el_editing = ask("What element do you want to edit? (#{@theme.keys.join(', ')}) leave blank to stop editing")
               continue if el_editing.blank?
               el_editing = el_editing.downcase.strip
-              continue unless theme.key?(el_editing)
-              theme[el_editing] = ask("#{el_editing}: (#{theme[el_editing]})") if theme.key?(el_editing)
+              continue unless @theme.key?(el_editing)
+              @theme[el_editing] = ask("#{el_editing}: (#{@theme[el_editing]})") if @theme.key?(el_editing)
             end
           end
         end
@@ -116,10 +116,10 @@ class UnovaForm::AssetsGenerator < Rails::Generators::NamedBase
       say "Generating stylesheet with schema: #{schema}, framework: #{framework} on destination: #{destination}"
 
       case framework
-        when "tailwind"
-          template "tailwind.#{schema}.erb", "#{destination}/unova_form.#{schema}", theme:
-        else
-          template "vanilla.#{schema}.erb", "#{destination}/unova_form.#{schema}", theme:
+      when "tailwind"
+        template "tailwind.#{schema}.erb", "#{destination}/unova_form.#{schema}"
+      else
+        template "vanilla.#{schema}.erb", "#{destination}/unova_form.#{schema}"
       end
 
       say "Stylesheet generated, trying to add it to your application css file"
