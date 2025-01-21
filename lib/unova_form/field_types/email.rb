78a62716d021564e7b4af2fd9ce473a029c475ef
RsMan-Dev <wily.richeroffi@gmail.com>
2025-01-21 10:36:44 +0100

    email pattern has been upgraded to match constraint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Regex_invalid_char_in_class

--- lib/unova_form/field_types/email.rb
+++ lib/unova_form/field_types/email.rb
@@ -5,9 +5,10 @@ module UnovaForm
     class Email < Base
       INPUT_TYPE = :email
 
+      # noinspection RegExpRedundantEscape - javascript needs the backslashes for pattern matching
       VALIDATORS = {
         # classic email validation like native html input email.
-        format: { with: /\A[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+\z/, message: :invalid_email_format }
+        format: { with: /\A[a-zA-Z0-9.!#$%&’*+\/=?^_`\{\|\}~\-]+@[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)+\z/, message: :invalid_email_format }
       }.freeze
     end
   end
