7e2ef7e853682c62fba2160902f4c0b950e19c1a
Paul Jacquemin <paul.jacquemin@unova.fr>
2026-04-13 15:17:49 +0200

    remove audio/wav

--- lib/unova_form/field_types/sound_file.rb
+++ lib/unova_form/field_types/sound_file.rb
@@ -6,7 +6,7 @@ module UnovaForm
       INPUT_TYPE = :file
 
       VALIDATORS = {
-        content_type: { in: %w[audio/mpeg audio/mp4 audio/ogg audio/wav audio/webm audio/x-m4a audio/x-aac audio/x-aiff audio/x-wav] },
+        content_type: { in: %w[audio/mpeg audio/mp4 audio/ogg audio/webm audio/x-m4a audio/x-aac audio/x-aiff audio/x-wav] },
         size: { less_than: 20.megabytes }
       }.freeze
     end
