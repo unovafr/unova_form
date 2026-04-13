de73ef29acbe06dcd827d8b240e7523c323d9af2
Paul Jacquemin <paul.jacquemin@unova.fr>
2026-04-13 14:26:58 +0200

    remove x-png content type

--- lib/unova_form/field_types/image_file.rb
+++ lib/unova_form/field_types/image_file.rb
@@ -6,7 +6,7 @@ module UnovaForm
       INPUT_TYPE = :file
 
       VALIDATORS = {
-        content_type: { in: %w[image/bmp image/jpeg image/x-png image/png image/webp image/gif] },
+        content_type: { in: %w[image/bmp image/jpeg image/png image/webp image/gif] },
         size: { less_than: 5.megabytes }
       }.freeze
     end
